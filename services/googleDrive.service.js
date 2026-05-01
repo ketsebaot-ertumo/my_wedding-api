bc8184587829dc4e5b312fe91f9aee5f09910e39
ketsebaot Ertumo <ertumoketsebaot@gmail.com>
2026-05-01 12:35:16 +0300

    test

--- services/googleDrive.service.js
+++ services/googleDrive.service.js
@@ -156,6 +156,11 @@ const { google } = require('googleapis');
 const fs = require('fs');
 const { Readable } = require('stream');
 
+const credentials = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString()
+);
+
+
 class GoogleDriveService {
     constructor() {
         this.drive = null;
@@ -168,11 +173,9 @@ class GoogleDriveService {
             // This automatically uses your gcloud credentials
             const auth = new google.auth.GoogleAuth({
                 // keyFile: process.env.GOOGLE_CREDENTIALS,
-                credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON),
-                // credentials: process.env.GOOGLE_CREDENTIALS_JSON
-                //     ? JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON)
-                //     : undefined,
+                // credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON),
                 // scopes: ['https://www.googleapis.com/auth/drive.file']
+                credentials,
                 scopes: ['https://www.googleapis.com/auth/drive']
             });
             const authClient = await auth.getClient();
