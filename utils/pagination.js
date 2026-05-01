045ae5b39598e720a0c93013413d2226db7e9881
ketsebaot Ertumo <ertumoketsebaot@gmail.com>
2026-05-02 01:07:57 +0300

    update sort order

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,19 +1,34 @@
+// exports.buildPagination = (query) => {
+//   const page = parseInt(query.page, 10) || 1;
+//   const limit = parseInt(query.limit, 10) || 10;
+
+//   const pagination = {
+//     page,
+//     limit,
+//     offset: (page - 1) * limit,
+//     where: {},
+//   };
+
+//   if (query.sort) {
+//     const sort = query.sort ? query.sort : 'createdAt';
+//     const order = query.order === "asc" ? "ASC" : "DESC";
+//     pagination.order = [[sort, order]];
+//   }
+
+//   return pagination;
+// };
 exports.buildPagination = (query) => {
   const page = parseInt(query.page, 10) || 1;
   const limit = parseInt(query.limit, 10) || 10;
 
-  const pagination = {
+  const sort = query.sort || 'createdAt';   // default sort
+  const order = query.order === "asc" ? "ASC" : "DESC"; // default DESC
+
+  return {
     page,
     limit,
     offset: (page - 1) * limit,
     where: {},
+    order: [[sort, order]],
   };
-
-  if (query.sort) {
-    const sort = query.sort ? query.sort : 'created_at';
-    const order = query.order === "asc" ? "ASC" : "DESC";
-    pagination.order = [[sort, order]];
-  }
-
-  return pagination;
 };
