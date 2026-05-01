478b0ea8b6c488ec91a03ea2ab6c6360f9ec8914
ketsebaot Ertumo <ertumoketsebaot@gmail.com>
2026-05-02 01:17:25 +0300

    final

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,22 +1,4 @@
-// exports.buildPagination = (query) => {
-//   const page = parseInt(query.page, 10) || 1;
-//   const limit = parseInt(query.limit, 10) || 10;
 
-//   const pagination = {
-//     page,
-//     limit,
-//     offset: (page - 1) * limit,
-//     where: {},
-//   };
-
-//   if (query.sort) {
-//     const sort = query.sort ? query.sort : 'createdAt';
-//     const order = query.order === "asc" ? "ASC" : "DESC";
-//     pagination.order = [[sort, order]];
-//   }
-
-//   return pagination;
-// };
 exports.buildPagination = (query) => {
   const page = parseInt(query.page, 10) || 1;
   const limit = parseInt(query.limit, 10) || 10;
